d8c55f535e1ced45bb20a554aa9e7a65797efbf8
Polkovnik-Daniil <0205danik@mail.ru>
2022-09-13 19:42:44 +0300

    version 0.1

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-#import pandas as pd
-#titanic = pd.read_csv('data/titanic.csv')
-#titanic["Age"].mean()
-#titanic[["Age","Fare"]].median()
-
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-students = pd.read_csv("data/student_data.csv")
-print(students["school"].mean())
-print(students[["school", "sex"]].median())
-print(students[["school", "sex"]].describe())
-print(students["avg_rating"].describe(percentiles=[0.05, 0.25, 0.5, 0.75]))
+students = pd.read_csv("data/Student_Marks.csv")
+print(students["Marks"].mean())
+print(students[["number_courses", "Marks"]].median())
+print(students[["number_courses", "Marks"]].describe())
+print(students["Marks"].describe(percentiles=[0.05, 0.25, 0.5, 0.75]))
 print(students.agg(
 {
-"school": ["max", "min", "median"],
-"sex": ["max", "min", "mean", "median"],
+"number_courses": ["max", "min", "median"],
+"Marks": ["max", "min", "mean", "median"],
 })
 )
-sns.histplot(data=students["avg_rating"], bins=10)
+sns.histplot(data=students["Marks"], bins=10)
 plt.show()
-sns.boxplot(x=students["avg_rating"])
+sns.boxplot(x=students["Marks"])
 plt.show()
-print(students[["year", "avg_rating"]].groupby("year").mean())
-print(students.groupby("year").mean())
-print(students.groupby(["year", "likes"])["avg_rating"].mean())
-print(students["year"].value_counts())
+print(students[["number_courses", "Marks"]].groupby("number_courses").mean())
+print(students.groupby("Marks").mean())
+print(students.groupby(["number_courses", "Marks"])["time_study"].mean())
+print(students["Marks"].value_counts())
